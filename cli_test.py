9fa63e7f41f99899819384610dc696c37273999e
Derek Grammer <33617359+dsgrammer@users.noreply.github.com>
2024-10-30 16:05:27 +0900

    improved the help message

--- cli_test.py
+++ cli_test.py
@@ -4,12 +4,15 @@ from pvsort.sort import Sort
 
 def main():
     parser = ArgumentParser()
-    group = parser.add_mutually_exclusive_group()
+    # group = parser.add_mutually_exclusive_group()
 
-    #parser.usage = "this is how you use the cli -h"
+    parser.usage = "cli_test.py [-h] sourceDir [targetDir] \n \
+        example: python3 cli_test.py 'C:/files'"
 
-    parser.add_argument('sourceDir', type=str, help="")
-    parser.add_argument('targetDir', type=str, help="", default="", nargs="?") # add default here for if nothing is provided make target the same as source
+    parser.add_argument('sourceDir', type=str, help="Source directory where the photos/videos that need to be sorted reside.")
+    # add default here for if nothing is provided make target the same as source
+    parser.add_argument('targetDir', type=str, help="Optional: Target directory where you want to sorted files. \
+                        If not provided target directory will default to source directory.", default="", nargs="?")
 
     args: Namespace = parser.parse_args()
     result1: str = args.sourceDir
